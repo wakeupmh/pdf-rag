55cf439eb1f2fd41dba5afac4a76708396ba2077
marcos.henrique <wakeupmh@gmail.com>
2024-08-29 21:24:54 -0300

    feat: add simple vector store

--- rag-app/app/api/chat/engine/chat.ts
+++ rag-app/app/api/chat/engine/chat.ts
@@ -1,6 +1,5 @@
 import { ContextChatEngine, Settings } from "llamaindex";
 import { getDataSource } from "./index";
-import { generateFilters } from "./queryFilter";
 
 export async function createChatEngine(documentIds?: string[], params?: any) {
   const index = await getDataSource(params);
@@ -11,7 +10,6 @@ export async function createChatEngine(documentIds?: string[], params?: any) {
   }
   const retriever = index.asRetriever({
     similarityTopK: process.env.TOP_K ? parseInt(process.env.TOP_K) : undefined,
-    filters: generateFilters(documentIds || []),
   });
 
   return new ContextChatEngine({
--- rag-app/app/api/chat/engine/index.ts
+++ rag-app/app/api/chat/engine/index.ts
@@ -1,18 +1,7 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
-import { VectorStoreIndex } from "llamaindex";
-import { PGVectorStore } from "llamaindex/storage/vectorStore/PGVectorStore";
-import {
-  PGVECTOR_SCHEMA,
-  PGVECTOR_TABLE,
-  checkRequiredEnvVars,
-} from "./shared";
+import { SimpleVectorStore, VectorStoreIndex } from "llamaindex";
 
 export async function getDataSource(params?: any) {
-  checkRequiredEnvVars();
-  const pgvs = new PGVectorStore({
-    connectionString: process.env.PG_CONNECTION_STRING,
-    schemaName: PGVECTOR_SCHEMA,
-    tableName: PGVECTOR_TABLE,
-  });
-  return await VectorStoreIndex.fromVectorStore(pgvs);
+ const simpleVectorStore = new SimpleVectorStore()
+  return await VectorStoreIndex.fromVectorStore(simpleVectorStore);
 }
