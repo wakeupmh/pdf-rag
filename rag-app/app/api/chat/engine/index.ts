b76d5250bfae78bf073280c8d34d31dcbd9a14d7
marcos.henrique <wakeupmh@gmail.com>
2024-08-31 14:25:09 -0300

    refactor: new arch

--- rag-app/app/api/chat/engine/index.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable turbo/no-undeclared-env-vars */
-import { SimpleVectorStore, VectorStoreIndex } from "llamaindex";
-
-export async function getDataSource(params?: any) {
- const simpleVectorStore = new SimpleVectorStore()
-  return await VectorStoreIndex.fromVectorStore(simpleVectorStore);
-}
