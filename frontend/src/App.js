6726074583e1b3540b966a231e8b763aa21a8147
marcos.henrique <wakeupmh@gmail.com>
2024-09-03 09:58:24 -0300

    refactor: move amplify outputs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import IconButton from "@mui/material/IconButton";
 import SendIcon from "@mui/icons-material/Send";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
-import outputs from "../../amplify_outputs.json";
+import outputs from "../amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
